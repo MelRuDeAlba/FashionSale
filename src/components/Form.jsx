8e1d1733f48562e2a145c0bc002a08c5d6533793
Melissa <yuukohlm@gmail.com>
2019-10-31 11:15:08 -0600

    simple form

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Form = () => (
+    <div className="Form">
+        <div className="Form-container">
+            <div className="Form-head">
+                <h2>Publica tu ropa</h2>
+            </div>
+            <div className="Form-item">
+                <form>
+                    <input name="title" type="text" placeholder="Titulo del anuncio" />
+                    <input name="descriotion" type="text" placeholder="Describe del anuncio" />
+                    <select name="type">
+                        <option disabled selected>Seleccionar</option>
+                        <option value="sale">Vender</option>
+                        <option value="donate">Donar</option>
+                    </select>
+                    <select name="gender">
+                        <option disabled selected>Seleccionar</option>
+                        <option value="neutral">Neutral</option>
+                        <option value="women">Dama</option>
+                        <option value="men">Caballero</option>
+                        <option value="girl">Niña</option>
+                        <option value="boy">Niño</option>
+                    </select>
+                    <input type="file" name="photo" />
+                    <button>Enviar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
+
+export default Form;
