e3eec7d4649686045157294123ef7df602870336
Melissa <yuukohlm@gmail.com>
2019-11-07 16:16:35 -0600

    add moment

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 const genderIcon = (gender) => {
     switch(gender) {
@@ -14,29 +15,37 @@ const genderIcon = (gender) => {
     }
 }
 
-const Item = props => (
-    <div className="Items">
-        <div 
-            className="Items-cover"
-            style={{
-                background: `url(${props.item.photo})`
-            }}
-        >
-            <span>
-                {genderIcon(props.item.gender)}
-            </span>
-            <div className="Items-type">
-                {props.item.sale ? 'Venta': 'Donación'}
+const Item = props => {
+    const date = moment(props.item.date, 'YYYYMMDD').fromNow();
+    return (
+        <div className="Items">
+            <div 
+                className="Items-cover"
+                style={{
+                    background: `url(${props.item.photo})`
+                }}
+            >
+                <span>
+                    {genderIcon(props.item.gender)}
+                </span>
+                <div className="Items-type">
+                    {props.item.sale ? 'Venta': 'Donación'}
+                </div>
+            </div>
+            <div className="Item-content">
+                <div className="Item-head">
+                    <h2>{props.item.title}</h2>
+                    <span>{props.item.gender}</span>
+                    
+                </div>
+                <p>{props.item.description}</p>
+                <div className="Item-date">
+                    <i class="fas fa-calendar-alt"></i>
+                    <span> - {date}</span>
+                </div>
             </div>
         </div>
-        <div className="Item-content">
-            <div className="Item-head">
-                <h2>{props.item.title}</h2>
-                <span>{props.item.gender}</span>
-                <span>{props.item.date}</span>
-            </div>
-        </div>
-    </div>
-);
+    )
+};
 
 export default Item;
